ae22b066a1ec0dc9fc4e52f09c42a24a10ca0ce9
Anna Canelas <canelasa2@gmail.com>
2017-04-09 23:24:26 +0000

    Allow users to delete a tag

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :notes do
     resources :tags, only: [:create]
   end
+  resources :tags, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
